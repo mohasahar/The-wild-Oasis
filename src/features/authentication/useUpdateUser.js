74fbbd17bbda370ec94206b00a0ab86c05507d00
Mohammed <moam.wxyz@gmail.com>
2024-04-22 12:27:02 +0300

    Update

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,18 +1,19 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import toast from "react-hot-toast";
-
+import { toast } from "react-hot-toast";
 import { updateCurrentUser } from "../../services/apiAuth";
 
 export function useUpdateUser() {
   const queryClient = useQueryClient();
+
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
-      toast.success(" User account successfully Updated");
-      //   queryClient.setQueryData(["user"], user);
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+    onSuccess: ({ user }) => {
+      toast.success("User account successfully Updated");
+
+      queryClient.setQueryData(["user"], user);
     },
     onError: (err) => toast.error(err.message),
   });
+
   return { updateUser, isUpdating };
 }

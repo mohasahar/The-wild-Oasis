7d5b765bbdf2cc76f36b83ef6ece7c35d0d125fe
Mohammed <moam.wxyz@gmail.com>
2024-04-22 11:15:48 +0300

    Update

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -82,13 +82,13 @@ function Pagination({ count }) {
   if (pageCount <= 1) return null;
   return (
     <StyledPagination>
-      <p>
+      <P>
         Showing<span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to
         <span>
           {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
         </span>
         of <span>{count}</span> results
-      </p>
+      </P>
       <Buttons>
         <PaginationButton onClick={prevPage} disabled={currentPage === 1}>
           <HiChevronLeft />
